a569c9757171c0257e640246bd9209b8b0036c41
Szerwigi1410 <szerwigi14@gmail.com>
2025-08-24 13:25:31 +0200

    improved the code, added colors

--- ascii-enc.py
+++ ascii-enc.py
@@ -1,20 +1,33 @@
 #!/usr/bin/env python
 
-print("Welcome to ascii encoder!")
+# Color codes
+GREEN="\033[32m" #
+RED="\033[31m"   #
+BLUE="\033[34m"  #
+CYAN="\033[36m"  #
+WHITE="\033[37m"
+YELLOW="\033[33m"#
+PURPLE="\033[35m"#
+BOLD="\033[1m"   #
+RESET="\033[0m"  #
+BLACK="\033[30m" 
+GRAY="\033[90m"
 
-question = input("Do you want to encode a sentence like a list? (y/n): ")
+print("Welcome to" + BOLD + GREEN + " a" + RED + "s" + BLUE + "c" + CYAN + "i" + PURPLE + "i " + WHITE + "encoder!" + RESET)
+
+question = input("Do you want to encode a sentence like a list? " + BOLD + "(y/n): " + RESET)
 
 
 if question == "y":
     #sentence entered by user
-    sentence = input("Enter your sentence: ")
+    sentence = input(BOLD + "Enter your sentence: " + RESET)
 
     for char in sentence:
         print(f"{char} -> {ord(char)}")
 
 else:
     
-    question1 = input("Do you want to encode a sentence like a string? (y/n): ")
+    question1 = input("Do you want to encode a sentence like a string? " + BOLD + "(y/n): ")
 
     if question1 =="n":
         print("Bye!")
@@ -22,7 +35,7 @@ else:
     
     else:
 
-        ascii = input("Enter your sentence: ")
+        ascii = input(BOLD + "Enter your sentence: " + RESET)
 
         ascii_str = "" # empty string to accumulate ascii codes
 
